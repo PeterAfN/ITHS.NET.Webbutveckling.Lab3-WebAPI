731c23f251357926beb4f3b23dc7284d8de2c0f4
peter palosaari <peterpalosaari@live.se>
2021-06-03 16:52:52 +0200

    create entities and datacontext

--- Api/Data/DataContext.cs
+++ Api/Data/DataContext.cs
@@ -1,10 +1,15 @@
 using System.Diagnostics.CodeAnalysis;
+using Api.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Data
 {
     public class DataContext : DbContext
     {
+        public DbSet<Kurser> Kurser { get; set; }
+
+        public DbSet<Deltagare> Deltagare { get; set; }
+
         public DataContext(DbContextOptions options) : base(options)
         {
         }
---
+++ Api/Data/Migrations/20210603144146_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Deltagare",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Förnamn = table.Column<string>(type: "TEXT", nullable: true),
+                    Efternamn = table.Column<string>(type: "TEXT", nullable: true),
+                    Epost = table.Column<string>(type: "TEXT", nullable: true),
+                    Mobilnummer = table.Column<string>(type: "TEXT", nullable: true),
+                    Adress = table.Column<string>(type: "TEXT", nullable: true),
+                    City = table.Column<string>(type: "TEXT", nullable: true),
+                    StateProvince = table.Column<string>(type: "TEXT", nullable: true),
+                    PostalCode = table.Column<int>(type: "INTEGER", nullable: false),
+                    Country = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deltagare", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kurser",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Kursnummer = table.Column<int>(type: "INTEGER", nullable: false),
+                    Kurstitel = table.Column<string>(type: "TEXT", nullable: true),
+                    Kursbeskrivning = table.Column<string>(type: "TEXT", nullable: true),
+                    Kurslängd = table.Column<int>(type: "INTEGER", nullable: false),
+                    Nivå = table.Column<string>(type: "TEXT", nullable: true),
+                    Status = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kurser", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Deltagare");
+
+            migrationBuilder.DropTable(
+                name: "Kurser");
+        }
+    }
+}
