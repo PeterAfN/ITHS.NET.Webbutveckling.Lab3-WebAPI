319a522b0e74c8899a3f1bd7845ab818f8d022e7
peter palosaari <peterpalosaari@live.se>
2021-06-04 16:48:00 +0200

    Finish class DeltagareController

--- Api/Controllers/DeltagareController.cs
+++ Api/Controllers/DeltagareController.cs
@@ -1,7 +1,9 @@
-
-using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
+using Api.Data;
+using Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
@@ -9,37 +11,87 @@ namespace Api.Controllers
     [Route("api/deltagare")]
     public class DeltagareController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public DeltagareController(DataContext context)
+        {
+            _context = context;
+        }
+
+
         [HttpGet()]
         public async Task<IActionResult> GetAllDeltagare()
         {
-            var data = new { id = "1", förnamn = "Peter" };
-            return Ok(data);
+            var result = await _context.Deltagare.ToListAsync();
+            return Ok(result);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetDeltagare(int id)
         {
-            var data = new { id = "1", förnamn = "Peter" };
-            return Ok(data);
+            try
+            {
+                var deltagare = await _context.Deltagare.SingleOrDefaultAsync(d => d.Id == id);
+                return Ok(deltagare);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPost()]
-        public async Task<IActionResult> AddDeltagare(Object model)
+        public async Task<IActionResult> AddDeltagare(Deltagare deltagare)
         {
-            return StatusCode(201);
+            try
+            {
+                _context.Deltagare.Add(deltagare);
+                var result = await _context.SaveChangesAsync();
+                return StatusCode(201);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateDeltagare(Int16 id, object model)
+        public async Task<IActionResult> UpdateDeltagare(int id, Deltagare deltagareModel)
         {
+            var deltagare = await _context.Deltagare.FindAsync(id);
+
+            deltagare.Förnamn = deltagareModel.Förnamn;
+            deltagare.Efternamn = deltagareModel.Efternamn;
+            deltagare.Epost = deltagareModel.Epost;
+            deltagare.Mobilnummer = deltagareModel.Mobilnummer;
+            deltagare.Adress = deltagareModel.Adress;
+            deltagare.City = deltagareModel.City;
+            deltagare.StateProvince = deltagareModel.StateProvince;
+            deltagare.PostalCode = deltagareModel.PostalCode;
+            deltagare.Country = deltagareModel.Country;
+            _context.Update(deltagare);
+            var result = await _context.SaveChangesAsync();
+
             return NoContent();
         }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteDeltagare(Int16 id)
+        public async Task<IActionResult> DeleteDeltagare(int id)
         {
+            try
+            {
+                var deltagare = await _context.Deltagare.SingleOrDefaultAsync(d => d.Id == id);
+                if (deltagare == null) return NotFound();
 
-            return NoContent();
+                _context.Deltagare.Remove(deltagare);
+                var result = _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
     }
